e524be0b743d65df68de6fa5bcc61ee21a585cf7
Kavya Parikh <61109166+kavya1526@users.noreply.github.com>
2023-11-27 18:07:12 -0700

    Update network-link-script.js

--- MC1/Network-Link-Graph/network-link-script.js
+++ MC1/Network-Link-Graph/network-link-script.js
@@ -2,7 +2,7 @@
 function processDataType(d) {
     for (var i = 0; i < d.length; i++) {
         d[i]['impOfPeople'] = parseFloat(d[i]['impOfPeople']);
-        d[i]['impOfSuborganization'] = parseFloat(d[i]['impOfSuborganization']);
+        d[i]['impOforganization'] = parseFloat(d[i]['impOforganization']);
         d[i]['clusterSize'] = parseFloat(d[i]['clusterSize']);
     }
     return d;
@@ -12,45 +12,53 @@ function processDataType(d) {
 function groupByOrganization(d) {
     const organizationClusters = [];
     d.forEach(element => {
-        if(!organizationClusters[element.suborganization]) {
-            organizationClusters[element.suborganization] = [];
+        if (!organizationClusters[element.organization]) {
+            organizationClusters[element.organization] = [];
         }
-        organizationClusters[element.suborganization].push(element);
+        organizationClusters[element.organization].push(element);
     });
     return organizationClusters;
 }
 
+// Function to clean the 'source' attribute in linksData
+function cleanLinksData(linksData) {
+    return linksData.map(d => ({
+        source: cleanSourceAttribute(d.source),
+        target: cleanSourceAttribute(d.target),
+        frequency: d.frequency
+    }));
+}
+
+// Function to clean the 'source' attribute (remove spaces and special characters)
+function cleanSourceAttribute(source) {
+    // Replace spaces and special characters with underscores, you can modify this based on your specific requirements
+    return source.replace(/[^\w]/g, '');
+}
+
 // Loading the data
 document.addEventListener('DOMContentLoaded', function () {
-    Promise.all([d3.csv('Resume-Data.csv')])
-        .then(function (values) {
-            resumeData = values[0];
-            resumeData = processDataType(resumeData);
-            nodeGroups = groupByOrganization(resumeData);
-            console.log(resumeData);
-            console.log(nodeGroups);
-            // plotGraph("board");
-            plotClusters(resumeData, nodeGroups);
-        });
+    Promise.all([
+        d3.csv('Resume-Data.csv'),
+        d3.csv('links.csv') // Load links data from the CSV file
+    ]).then(function (values) {
+        const resumeData = processDataType(values[0]);
+        const nodeGroups = groupByOrganization(resumeData);
+        const linksData = cleanLinksData(values[1]);
+        console.log(resumeData);
+        console.log(nodeGroups);
+        console.log(linksData);
+        plotClusters(resumeData, nodeGroups, linksData);
+    });
 });
 
-/*
-function plotGraph(type) {
-    console.log(type);
-    if (type == 'board') {
-        zoomBoard();
-    }
-    else if (type == 'technical') {
-        zoomTechnical();
-    }
-}
-*/
 
-function plotClusters(resumeData, nodeGroups) {
 
-    const margin = { top: 30, right: 30, bottom: 120, left: 60 },
+function plotClusters(resumeData, nodeGroups, linksData) {
+    console.log(linksData);
+
+    const margin = { top: 50, right: 30, bottom: 120, left: 60 },
         width = 750 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        height = 750 - margin.top - margin.bottom;
 
     const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
@@ -62,50 +70,67 @@ function plotClusters(resumeData, nodeGroups) {
 
     const tooltip = d3.select("#plotDiv")
         .append("div")
-        .attr("class", "d3-tooltip")
-        .style("position", "absolute")
-        .style("opacity", "0")
-        .style("padding", "5px")
-        .style("color", "black")
-        .style("background", "white")
-        .style("border", "2px solid #000000")
-        .style("stroke", "black")
-        .style("stroke-width", "2px")
-        .style("border-radius", "5px")
+        .attr("class", "d3-tooltip");
 
-    // var stimulationTask = d3.forceSimulation(resumeData)
-    //     .force("x", d3.forceX(Math.random()*width))
-    //     .force("y", d3.forceY(Math.random()*height.strength(0.1)))
-    //     .force("collide", d3.forceCollide())
-    //     .stop();
+    // Function to compute dynamic attributes for main circles
+    const getcirclePositionAttributes = (organization) => {
+        const pentagonRadius = 280; // Adjust the radius as needed
+        const angleOffset = Math.PI / 2; // Offset to start the pentagon from the top
+        const angleStep = (2 * Math.PI) / 5; // Angle between vertices
 
-    svg.selectAll("circle")
-        .data(Object.keys(nodeGroups))
-        .enter().append("g")
-        .selectAll("circle")
-        .data(d => nodeGroups[d])
+        const positions = {
+            'POKMember': { angle: 0 },
+            'GASTechCommittee': { angle: angleStep },
+            'GASTechBoard': { angle: 2 * angleStep },
+            'GASTechEmployee': { angle: 3 * angleStep },
+            'POKCommittee': { angle: 4 * angleStep },
+        };
+
+        return {
+            cx: 0.5 * width + pentagonRadius * Math.cos(angleOffset + positions[organization].angle),
+            cy: 0.5 * height + pentagonRadius * Math.sin(angleOffset + positions[organization].angle),
+            r: 70, // Adjust the radius as needed
+        };
+    };
+
+    // Create a force simulation for the smaller circles
+    const simulation = d3.forceSimulation(resumeData)
+        .force("x", d3.forceX().strength(0.05).x(d => {
+            const organization = d.organization;
+            const circlePosition = getcirclePositionAttributes(organization);
+            return circlePosition.cx; // Use the main circle's attributes directly
+        }))
+        .force("y", d3.forceY().strength(0.05).y(d => {
+            const organization = d.organization;
+            const circlePosition = getcirclePositionAttributes(organization);
+            return circlePosition.cy; // Use the main circle's attributes directly
+        }))
+        .force("collide", d3.forceCollide().radius(d => (d.impOfPeople / 9) * 20 + 9))
+        .on("tick", () => {
+            circles.attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+        });
+
+    // Create smaller circles inside the main circles
+    const circles = svg.selectAll(".node")
+        .data(resumeData)
         .enter().append("circle")
+        .attr("class", "node")
         .attr("stroke", "black")
         .attr("stroke-width", 2)
-        .attr("cx", function(d) {
-            if(d.suborganization == 'Board') {return 0.1 * width}
-            if(d.suborganization == 'Technical') {return 0.3 * width}
-            if(d.suborganization == 'Security') {return 0.5 * width}
-            if(d.suborganization == 'Executive') {return 0.7 * width}
-            if(d.suborganization == 'Operations') {return 0.9 * width}
-        })
-        .attr("cy", function(d) {
-            if(d.suborganization == 'Board') {return 0.7 * height}
-            if(d.suborganization == 'Technical') {return 0.3 * height}
-            if(d.suborganization == 'Security') {return 0.9 * height}
-            if(d.suborganization == 'Executive') {return 0.1 * height}
-            if(d.suborganization == 'Operations') {return 0.5 * height}
-        })
-        .attr("fill", d => colorScale(d.suborganization))
-        .attr("r", d => (d.impOfPeople / 9) * 30)
+        .attr("fill", d => colorScale(d.organization))
+        .attr("r", d => ((d.impOfPeople + 2) / 9) * 20)
         .on("mouseover", function (event, d) {
+            // Fade all other elements
+            svg.selectAll(".link")
+                .transition()
+                .style("opacity", link => (link.source === d || link.target === d) ? 1 : 0.1);
+
+            circles.transition()
+                .style("opacity", node => (node === d) ? 1 : 0.1);
+
             // Show tooltip on mouseover
-            tooltip.html(`Name : ${(d.name)} <br>  Role : ${(d.role)} <br>  Suborganization : ${(d.suborganization)} ` )
+            tooltip.html(`Name: ${d.name} <br>Role: ${d.role} <br>Organization: ${d.organization}`)
                 .style("opacity", 0.9)
                 .style("left", (event.pageX + 15) + "px")
                 .style("top", (event.pageY - 15) + "px");
@@ -115,110 +140,17 @@ function plotClusters(resumeData, nodeGroups) {
             tooltip.style("left", (event.pageX + 15) + "px")
                 .style("top", (event.pageY - 15) + "px");
         })
-        .on("mouseout", function (event) {
+        .on("mouseout", function () {
+            // Restore opacity on mouseout
+            svg.selectAll(".link")
+                .transition()
+                .style("opacity", 1);
+
+            circles.transition()
+                .style("opacity", 1);
+
             // Hide tooltip on mouseout
-            tooltip.html(``)
+            tooltip.html("")
                 .style("opacity", 0);
         });
-
-        
 }
-
-// function plotGraph() {
-//     console.log("Welcome to Board");
-
-//     d3.selectAll(".circle").remove()
-//     d3.selectAll(".text").remove();
-
-//     const margin = { top: 30, right: 30, bottom: 120, left: 60 },
-//         width = 750 - margin.left - margin.right,
-//         height = 500 - margin.top - margin.bottom;
-
-//     const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-
-//     /*
-//     const strokeScale = d3.scaleLinear()
-//         .domain([d3.min(resumeData, d => d.impOfSuborganization), d3.max(resumeData, d => d.impOfSuborganization)])
-//         .range([1, 6]); // Adjust stroke width range based on impOfSuborganization
-//     */
-
-//     const svg = d3.select("#plotSvg")
-//         .attr("width", width + margin.left + margin.right)
-//         .attr("height", height + margin.top + margin.bottom)
-//         .append("g")
-//         .attr("transform", `translate(${margin.left},${margin.top})`);
-
-//     data = resumeData;
-
-//     const tooltip = d3.select("#plotDiv")
-//         .append("div")
-//         .attr("class", "d3-tooltip")
-//         .style("position", "absolute")
-//         .style("opacity", "0")
-//         .style("padding", "5px")
-//         .style("color", "black")
-//         .style("background", "white")
-//         .style("border", "2px solid #000000")
-//         .style("stroke", "black")
-//         .style("stroke-width", "2px")
-//         .style("border-radius", "5px")
-
-//     const x = d3.scaleBand()
-//         .range([0, width])
-//         .domain(data.map(d => d.name))
-//         .padding(0.2);
-//     svg.append("g")
-//         .classed("scatter_x", true)
-//         .attr("transform", `translate(0, ${height})`)
-//         .call(d3.axisBottom(x))
-//         .selectAll("text")
-//         .attr("transform", "translate(-12,10)rotate(-90)")
-//         .style("text-anchor", "end");
-
-//     const y = d3.scaleLinear()
-//         .domain([0, d3.max(data, d => d.impOfPeople)]) // Adjusted y domain
-//         .range([height, 0]);
-//     svg.append("g")
-//         .classed("scatter_y", true)
-//         .call(d3.axisLeft(y));
-
-//     svg.append("g")
-//         .classed("scatter", true)
-//         .selectAll("circle")
-//         .data(data)
-//         .join("circle")
-//         .attr("cx", d => x(d.name) + x.bandwidth() / 2) // Adjusted x position for circles
-//         .attr("cy", height/2) // Fixed y position at the bottom of the chart
-//         .attr("r", d => d.impOfPeople * 3) // Adjusted radius based on impOfPeople
-//         .attr("fill", d => colorScale(d.impOfPeople))
-//         .attr("stroke", "black") // Border color 
-//         .attr("stroke-width", "1") // Border width 
-//         .attr("opacity", 1)
-//         .classed("box_circles", true)
-//         .on("mouseover", function (event, d) {
-    
-//             // Show tooltip on mouseover
-//             tooltip.html(`Name : ${(d.name)} <br>  Importance of Person : ${(d.impOfPeople)} <br>  Importance of Suborganization : ${(d.impOfSuborganization)} ` )
-//                 .style("opacity", 0.9)
-//                 .style("left", (event.pageX + 15) + "px")
-//                 .style("top", (event.pageY - 15) + "px");
-//         })
-//         .on("mousemove", function (event) {
-//             // Move tooltip with the mouse
-//             tooltip.style("left", (event.pageX + 15) + "px")
-//                 .style("top", (event.pageY - 15) + "px");
-//         })
-//         .on("mouseout", function (event) {
-//             // Hide tooltip on mouseout
-//             tooltip.html(``)
-//                 .style("opacity", 0);
-//         });
-
-//     svg.append("text")
-//         .classed("scatter_text", true)
-//         .attr("x", (width / 2))
-//         .attr("y", (margin.top - 25))
-//         .attr("text-anchor", "middle")
-//         .style("font-size", "16px")
-//         .text("Relationship between Sub-organizations");
-// }
